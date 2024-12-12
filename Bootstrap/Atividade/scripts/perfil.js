6ed4e7ee707fc06ccef6ff442c13f798b888f249
Arvicx <arthurfernandesrodrigues1@gmail.com>
2024-12-12 14:35:58 -0300

    cinemart

---
+++ Bootstrap/Atividade/scripts/perfil.js
@@ -0,0 +1,12 @@
+usuario = document.getElementById('submit').value
+senha = document.getElementById('submit').value
+
+function login(){
+    if(usuario == admin && senha == 1234){
+        alert("Login Efetuado!")
+    }else{
+        alert("kkkkkkkkkkkkk errou")
+    }
+}
+
+login()
